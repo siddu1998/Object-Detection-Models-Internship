2890b46cba3912eb31d28d76d474e565b0621069
Sai Siddartha Maram <msaisiddartha1@gmail.com>
2018-06-07 21:48:27 +0530

    Error resolved
    Edited System.cc use if needed

--- c.cpp
+++ c.cpp
@@ -1,22 +1,3 @@
-/**
-* This file is part of ORB-SLAM2.
-*
-* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
-* For more information see <https://github.com/raulmur/ORB_SLAM2>
-*
-* ORB-SLAM2 is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM2 is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
-*/
 
 
 #include<iostream>
@@ -55,7 +36,6 @@ int main(int argc, char **argv)
         return 1;
     }    
 
-    // Create SLAM system. It initializes all system threads and gets ready to process frames.
     ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
 
     ImageGrabber igb(&SLAM);
@@ -65,14 +45,14 @@ int main(int argc, char **argv)
 
     ros::spin();
 
-    // Stop all threads
+
     SLAM.Shutdown();
 
-	// Save 3D points as obj file
+	
 	SLAM.getMap()->Save("cam_map_pts_out.obj");
-	// Save 3D points and timestamps of all keyframes they are visible in
+	
 	SLAM.getMap()->SaveWithTimestamps("cam_map_pts_and_keyframes.txt");
-	// Save camera trajectory
+
 	SLAM.SaveKeyFrameTrajectoryTUM("cam_key_frame_trajectory.txt");
 
     ros::shutdown();
@@ -82,7 +62,7 @@ int main(int argc, char **argv)
 
 void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg)
 {
-    // Copy the ros image message to cv::Mat.
+
     cv_bridge::CvImageConstPtr cv_ptr;
     try
     {
