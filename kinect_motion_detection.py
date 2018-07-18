d94fae81cfff9f70b65f30b8c8ad9eacaa240385
Sai Siddartha Maram <msaisiddartha1@gmail.com>
2018-07-18 12:46:14 +0530

    Arduino Knowledge Transfer

--- kinect_motion_detection.py
+++ kinect_motion_detection.py
@@ -19,16 +19,24 @@ cv2.resizeWindow('Navig',400,100)
 cv2.moveWindow('Navig',700,5)
 #cv2.namedWindow('Fin')
 #cv2.namedWindow('Win')
+
+#Establishing Kernel for Image smoothing
 kernel = np.ones((5, 5), np.uint8)
 
 
 print('Press \'b\' in window to stop')
+
+
+#Create Trackbars for establishing various parameters
 cv2.createTrackbar('val1', 'Video', 37, 1000, nothing)
 cv2.createTrackbar('val2', 'Video', 43, 1000, nothing)
 cv2.createTrackbar('bin', 'Video',20,50,nothing)
 cv2.createTrackbar('erode', 'Video',4,10,nothing)#after plenty of testing
+#Incase of no object
 imn=cv2.imread('blank.bmp')
 
+
+#clip depth and get pixel depth image
 def pretty_depth(depth):
     np.clip(depth, 0, 2**10 - 1, depth)
     depth >>= 2
@@ -36,7 +44,7 @@ def pretty_depth(depth):
 
     return depth
 
-
+#while depth image exists calculate the distance from camera link to base link
 while 1:
     imm=cv2.imread('blank.bmp')
     cv2.imshow('Navig',imn)
@@ -47,20 +55,22 @@ while 1:
     f10=0
     f8=0
     dst = pretty_depth(freenect.sync_get_depth()[0])#input from kinect
-    #cv2.flip(dst,0,dst)
-    #cv2.flip(dst,1,dst)
+    #create frame of rectangular dimenstions
     cv2.rectangle(dst,(0,0),(640,480),(40,100,0),2)
+    #get trackbar positions
     binn=cv2.getTrackbarPos('bin', 'Video')
     e=cv2.getTrackbarPos('erode', 'Video')
     dst=(dst/binn)*binn
+    #perform image smoothin
     dst=cv2.erode(dst,kernel,iterations=e)
 
     v1=37
     v2=43
     v1 = cv2.getTrackbarPos('val1', 'Video')
     v2 = cv2.getTrackbarPos('val2', 'Video')
+    #canny based edge detection
     edges = cv2.Canny(dst, v1, v2)
-
+    #obtain contours from detected edges
     _,contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(dst, contours, -1, (0, 0, 255), -1)
 
@@ -70,6 +80,7 @@ while 1:
     cv2.createTrackbar('spacing', 'Video', 30, 100, nothing)
     spac=cv2.getTrackbarPos('spacing', 'Video')
     (rows,cols)=dst.shape
+    #distribute the whole frame nto a grid
     for i in range(rows/spac):
         for j in range(cols/spac):
             cv2.circle(dst, (spac*j,spac*i), 1, (0, 255, 0), 1)
@@ -113,6 +124,7 @@ while 1:
         elif(f12==1):
 	        cv2.putText(dst," back",(325,90),cv2.FONT_HERSHEY_DUPLEX,1,(2),1)
 		arduino.encode('mb'.encode())
+	
 		
         cv2.line(dst,(130,0),(130,480),(0),1)
         cv2.line(dst,(320,0),(320,480),(0),1)
